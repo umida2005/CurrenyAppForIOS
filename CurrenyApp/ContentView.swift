6d0e67f35e0fa069c4e9242748dd7e852f62eb7b
mac <@umida_azimova>
2025-09-15 21:28:44 +0500

    Initial Commit

---
+++ CurrenyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CurrenyApp
+//
+//  Created by mac on 15/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CurrenyApp/CurrenyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrenyAppApp.swift
+//  CurrenyApp
+//
+//  Created by mac on 15/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CurrenyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
